8e374085ca3c98afc6bcade51d4b647cef600b20
andrearias21 <andrea.arias.mt@usa.edu.co>
2022-10-17 20:47:20 -0500

    first commit

---
+++ src/main/java/com/example/demo/Reto5G40Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Reto5G40Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Reto5G40Application.class, args);
+	}
+
+}
